ec79a8d598968ee428d04476f9fbb3ae22d4a2d2
krypek <krypek@tuta.io>
2025-03-31 22:52:11 +0200

    Fix lebab errors

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ export function assert(v: any, msg?: string): asserts v {
     if (!v) throw new Error(`Assertion error${msg ? `: ${msg}` : ''}`)
 }
 import ts, { SyntaxKind } from 'typescript'
-// @ts-expect-error
 import * as lebab from 'lebab'
 import * as fs from 'fs'
 import * as path from 'path'
@@ -19,31 +18,37 @@ const outTypesPath: string = './typedefs.json'
 async function applyLebab() {
     const outPath = './game.compiled.lebab.js'
     if (!fileExists(outPath)) {
-        console.log('running lebab...')
         const orig = await fs.promises.readFile(gameCompiledPath, 'utf8')
-        let { code }: { code: string; warnings: { type: string; line: number; msg: string }[] } = lebab.transform(
-            orig,
-            [
-                // 'class',
-                'template',
-                'arrow',
-                'arrow-return',
-                'let',
-                'default-param',
-                // 'destruct-param',
-                'arg-spread',
-                'arg-rest',
-                'obj-method',
-                'obj-shorthand',
-                // 'no-strict',
-                // 'commonjs',
-                'exponent',
-                'multi-var',
-                'for-of',
-                // 'for-each',
-                'includes',
-            ]
-        )
+        let code = orig
+
+        /* this has to split into multiple passes because of https://github.com/lebab/lebab/issues/205 */
+        console.log('running lebab pass 1...')
+        code = lebab.transform(code, ['multi-var']).code
+        console.log('running lebab pass 2...')
+        code = lebab.transform(code, ['multi-var']).code
+        // type res = { code: string; warnings: { type: string; line: number; msg: string }[] }
+        console.log('running lebab pass 3...')
+        code = lebab.transform(code, [
+            // 'class',
+            'template',
+            'arrow',
+            'arrow-return',
+            'let',
+            'default-param',
+            // 'destruct-param',
+            'arg-spread',
+            'arg-rest',
+            'obj-method',
+            'obj-shorthand',
+            // 'no-strict',
+            // 'commonjs',
+            'exponent',
+            'for-of',
+            // 'for-each',
+            'includes',
+        ]).code
+        code = code.replace(/window\.ig\.Class = \(\) =\> \{\}/, 'window.ig.Class = function() {}')
+        code = code.replace(/let g;(\s+if \(this.attackCounter \<= 3)/, 'var g;$1')
         await fs.promises.writeFile(outPath, code, 'utf8')
     }
     gameCompiledPath = outPath
