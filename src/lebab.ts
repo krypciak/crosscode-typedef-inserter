9cb1a444ea3e8195799a991edb8221a443818334
krypek <krypek@tuta.io>
2026-07-20 23:07:58 +0200

    Run prettier before running lebab

--- src/lebab.ts
+++ src/lebab.ts
@@ -2,8 +2,11 @@
 import * as lebab from 'lebab'
 import * as fs from 'fs'
 import { fileExists } from './misc'
+import * as prettier from 'prettier'
+import * as babelParser from 'prettier/plugins/babel'
+import * as estreeParser from 'prettier/plugins/estree'
 
-function applyLebabToCode(origCode: string) {
+export function applyLebabToCode(origCode: string) {
     let code = origCode
 
     /* this has to split into multiple passes because of https://github.com/lebab/lebab/issues/205 */
@@ -39,11 +42,32 @@ function applyLebabToCode(origCode: string) {
     return code
 }
 
+async function runPrettier(code: string) {
+    console.log('running prettier')
+    const formattedCode = await prettier.format(code, {
+        parser: 'babel',
+        plugins: [babelParser, estreeParser],
+
+        semi: false,
+        useTabs: false,
+        tabWidth: 4,
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+        quoteProps: 'as-needed',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+    })
+    return formattedCode
+}
+
 export async function applyLebab(gameCompiledPath: string): Promise<string> {
     const outPath = './game-compiled/game.compiled.lebab.js'
     if (!(await fileExists(outPath))) {
         const origCode = await fs.promises.readFile(gameCompiledPath, 'utf8')
-        const newCode = applyLebabToCode(origCode)
+        const formattedCode = await runPrettier(origCode)
+
+        const newCode = applyLebabToCode(formattedCode)
         await fs.promises.writeFile(outPath, newCode)
     }
     return outPath
